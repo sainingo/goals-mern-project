31ababd2b699cd1209fcb5452b5ae9c2f9fd4b89
sainingo <nickmentor421@gmail.com>
2022-09-22 15:27:13 +0300

    Authorize & authenticate user

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,11 +1,14 @@
 const asyncHandler = require('express-async-handler')
 
 const Goal = require('../models/goalModel')
+const User = require('../models/userModel')
+
 
 
 const getGoals = asyncHandler(async (req, res) => {
 
-    const goals = await Goal.find()
+    const goals = await Goal.find({ user: req.user.id})
+
     res.status(200).json(goals)
 })
 
@@ -15,7 +18,8 @@ const setGoals = asyncHandler(async (req, res) => {
         throw new Error('please add a text field')
     }
     const goal = await Goal.create({
-        text: req.body.text
+        text: req.body.text,
+        user: req.user.id,
     })
     res.status(200).json(goal)
 })
@@ -27,6 +31,21 @@ const updateGoals = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error('Goal not found')
     }
+
+    const user = await User.findById(req.user.id)
+    //check if user exists
+    if(!user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    //only logged in user matches goal user
+    if(goal.user.toString() !== user.id) {
+        res.status(401)
+        throw new Error('User not authorized')
+    }
+
+
     const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true,})
     res.status(200).json(updatedGoal)
 })
@@ -38,6 +57,21 @@ const deleteGoals = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error('Goal not Found')
     }
+
+    const user = await User.findById(req.user.id)
+    //check if user exists
+    if(!user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    //only logged in user matches goal user
+    if(goal.user.toString() !== user.id) {
+        res.status(401)
+        throw new Error('User not authorized')
+    }
+
+
     // const deletedGoal = await Goal.findByIdAndDelete(req.params.id)
     await goal.remove()
     res.status(200).json({id: req.params.id})
--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const { getGoals, setGoals, updateGoals, deleteGoals } = require('../controllers/goalController')
 
+const { protect } = require('../middleware/authMiddleware')
+
 const router = express.Router()
 
-router.route('/').get(getGoals).post(setGoals)
-router.route('/:id').put(updateGoals).delete(deleteGoals)
+router.route('/').get(protect, getGoals).post(protect, setGoals)
+router.route('/:id').put(protect, updateGoals).delete(protect, deleteGoals)
 
 
 module.exports = router
