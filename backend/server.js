43db8283949d53439672ed2def5b6a378ca8763e
sainingo <nickmentor421@gmail.com>
2022-09-22 09:54:08 +0300

    Setup routes endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log('hello')
+const express = require('express')
+const dotenv = require('dotenv').config()
+
+
+const port = process.env.PORT || 5000
+
+const app = express()
+
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+
+app.listen(port, () => console.log(`server running on port ${port}`))
