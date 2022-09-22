b30388b0804ddb945127878a489433585cf8b521
sainingo <nickmentor421@gmail.com>
2022-09-22 17:44:12 +0300

    Style form inputs

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -1,6 +1,24 @@
+import {FaSignInAlt, FaSignOutAlt, FaUser} from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+
+
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='header'>
+        <div className='logo'>
+            <Link to="/">GoalSetter</Link>
+        </div>
+
+        <ul>
+            <li>
+                <Link to="/login"><FaSignInAlt />Login</Link>
+            </li>
+            <li>
+                <Link to="/Register"><FaUser />Register</Link>
+            </li>
+        </ul>
+    </header>
   )
 }
 
--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,8 +1,63 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { FaSignInAlt} from 'react-icons/fa'
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  })
+
+  const { email, password } = formData
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
   return (
-    <div>Login</div>
+    <>
+    <section className='heading'>
+      <h1>
+        <FaSignInAlt />Login
+      </h1>
+      <p>Login and start setting goal</p>
+    </section>
+
+    <section className='form'>
+        <form onSubmit={onSubmit}>
+         
+          <div className='form-group'>
+          <input type="email" 
+          className="form-control" 
+          id="email" 
+          name="email" 
+          value={email} 
+          placeholder="Enter Your Email" 
+          onChange={onChange}/>
+          </div>
+
+          <div className='form-group'>
+          <input type="password" 
+          className="form-control" 
+          id="password" 
+          name="password" 
+          value={password} 
+          placeholder="Enter Your Password" 
+          onChange={onChange}/>
+          </div>
+
+          <div className='form-group'>
+            <button type='submit' className='btn btn-block'>Submit</button>
+          </div>
+        </form>
+    </section>
+    </>
   )
 }
 
