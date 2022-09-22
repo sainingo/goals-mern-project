79bbf6fdbb87bcba1167841f00f9f41032368686
sainingo <nickmentor421@gmail.com>
2022-09-22 12:26:51 +0300

    Add user routes

---
+++ backend/controllers/userController.js
@@ -0,0 +1,20 @@
+const registerUser = (req, res) => {
+    res.json({message: 'Register user'})
+}
+
+
+const loginUser = (req, res) => {
+    res.json({message: 'Login user'})
+}
+
+
+const getMe = (req, res) => {
+    res.json({message: 'User data display'})
+}
+
+
+module.exports = {
+    registerUser,
+    loginUser,
+    getMe,
+}
