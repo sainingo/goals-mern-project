df40510c1d43e42e28b7b37aeb44d733423310cf
sainingo <nickmentor421@gmail.com>
2022-09-23 15:40:14 +0300

    Login validation

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -1,9 +1,19 @@
 import {FaSignInAlt, FaSignOutAlt, FaUser} from 'react-icons/fa'
-import { Link } from 'react-router-dom'
-
+import { Link, useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { logout, reset } from '../features/auth/authSlice'
 
 
 const Header = () => {
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const {user} = useSelector((state) => state.auth)
+
+    const onLogout = () => {
+        dispatch(logout())
+        dispatch(reset())
+        navigate('/')
+    }
   return (
     <header className='header'>
         <div className='logo'>
@@ -11,12 +21,19 @@ const Header = () => {
         </div>
 
         <ul>
-            <li>
-                <Link to="/login"><FaSignInAlt />Login</Link>
+            {user ? (
+                <li>
+                <button className='btn' onClick={onLogout}><FaSignOutAlt />Logout</button>
             </li>
+            ) : (<>
             <li>
-                <Link to="/Register"><FaUser />Register</Link>
-            </li>
+            <Link to="/login"><FaSignInAlt />Login</Link>
+        </li>
+        <li>
+            <Link to="/Register"><FaUser />Register</Link>
+        </li>
+            </>)}
+            
         </ul>
     </header>
   )
--- front-end/src/features/auth/authService.js
+++ front-end/src/features/auth/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = '/api/users'
+const API_URL = '/api/users/'
 
 //register user
 const register = async (userData) => {
@@ -13,8 +13,27 @@ const register = async (userData) => {
     return response.data
 }
 
+//login
+const login = async (userData) => {
+    const response = await axios.post(API_URL + 'login', userData)
+
+    if (response.data) {
+        localStorage.setItem('user', JSON.stringify(response.data))
+    }
+
+    return response.data
+}
+
+//logout
+
+const logout = async () => {
+    localStorage.removeItem('user')
+}
+
 const authService = {
-    register
+    register,
+    logout,
+    login,
 }
 
 export default authService
