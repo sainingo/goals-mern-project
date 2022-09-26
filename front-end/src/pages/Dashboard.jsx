f69ee42a015237b54a02fc5d6808b7e1e8797c8f
sainingo <nickmentor421@gmail.com>
2022-09-26 10:27:50 +0300

    Protected routes

--- front-end/src/pages/Dashboard.jsx
+++ front-end/src/pages/Dashboard.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React,{ useEffect}from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
 
 const Dashboard = () => {
+  const {user} = useSelector((state) => state.auth)
+  const navigate = useNavigate()
+  
+  useEffect(() => {
+    if(!user) {
+      navigate('/login')
+    }
+  }, [navigate, user])
+
+
+
   return (
     <div>Dashboard</div>
   )
