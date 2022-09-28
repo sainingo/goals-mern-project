66fee82ac49d9825a274cc57547cf147bd0de85a
sainingo <nickmentor421@gmail.com>
2022-09-28 09:48:56 +0300

    Create goal form submission

---
+++ front-end/src/features/goals/goalSlice.js
@@ -0,0 +1,27 @@
+import { createSlice, createAsyncThunk} from '@reduxjs/toolkit'
+
+
+const initialState = {
+    goals: [],
+    isError: false,
+    isSuccess: false,
+    isLoading: false,
+    message: ''
+}
+
+export const goalSlice  = createSlice({
+   name: 'goals',
+   initialState,
+   reducers: {
+       reset: (state) => initialState
+   },
+   extraReducers: (builder) => {
+       
+   }
+
+})
+
+
+export const {reset} = goalSlice.actions
+
+export default goalSlice.reducer
--- front-end/src/pages/Dashboard.jsx
+++ front-end/src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React,{ useEffect}from 'react'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
+import GoalForm from '../components/GoalForm'
 
 
 const Dashboard = () => {
@@ -16,7 +17,13 @@ const Dashboard = () => {
 
 
   return (
-    <div>Dashboard</div>
+    <>
+      <section className='heading'>
+        <h1>Welcome {user && user.name}</h1>
+        <p>Goals Dashboard</p>
+      </section>
+      <GoalForm />
+    </>
   )
 }
 
